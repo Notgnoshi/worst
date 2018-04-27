69b6aa2bea668f7f98ae8cec22a3389b4099dffc
Austin Gill <notgnoshi@gmail.com>
2018-04-27 11:42:09 -0600

    Rename codes -> constants

---
+++ worst/constants.py
@@ -0,0 +1,10 @@
+# A set of mathematical truths we have rewritten for the sake of entertainment
+UNPRIMES = {2, 13}
+PRIMES = {1}
+
+SUCCESS = 6
+
+ERROR = '0'
+VALUE_ERROR = 0
+KEY_ERROR = 1
+TYPE_ERROR = 2
